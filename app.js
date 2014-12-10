20a8320642bf4ee24ac0e5cbce9efb39fc3aebde
Eric Sabelhaus <eric.sabelhaus@gmail.com>
2014-12-10 11:01:16 -0500

    scratch that, just going to use nginx to handle SSL

--- app.js
+++ app.js
@@ -1,34 +1,16 @@
 var express = require('express'),
     app = express(),
-    http = require('http'),
-    https = require('https'),
-    forceSSL = require('express-force-ssl'),
     path = require('path'),
-    fs = require('fs'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
     nconf = require('nconf'),
     routes = require('./routes/index');
 
-
 (function(){
   "use strict";
 
 nconf.file({ file: 'config/prestige.json' });
-
-var server;
-
-if (nconf.get('config:ssl:enabled') === true) {
-  var ssl_options = {
-    key: fs.readFileSync(nconf.get('config:ssl:ssl_client_key')),
-    cert: fs.readFileSync(nconf.get('config:ssl:ssl_client_cert')),
-    ca: fs.readFileSync(nconf.get('config:ssl:ssl_ca_cert'))
-  }
-  server = https.createServer(ssl_options, app);
-} else {
-  server = http.createServer(app);
-}
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
@@ -39,6 +21,6 @@ app.use('/', routes);
 
 module.exports = app;
 
-server.listen(nconf.get('config:port'));
+app.listen(nconf.get('config:port'));
 
 })();
